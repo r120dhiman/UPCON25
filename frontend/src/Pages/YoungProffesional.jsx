0d9ade4049b6a743104d59ce4a9dd9006bb64d08
ROHIT KUMAR <r120dhiman@gmail.com>
2025-11-13 15:36:07 +0530

    updated young professional page

--- frontend/src/Pages/YoungProffesional.jsx
+++ frontend/src/Pages/YoungProffesional.jsx
@@ -2,28 +2,22 @@ import React from 'react'
 import SpeakerCard from '../components/SpeakerCard'
 
 function YoungProffesional() {
-  const speakerData1 = {
-    imageUrl: "/Speakers/young/gururaj.jpeg",
-    name: "Prof. Gururaj Mirle Vishwanath",
-    institution: "IIT Kanpur",
-    // talkTitle: "Primary efforts in applying counter manipulation detection technology",
-  };
   const speakerData2 = {
     imageUrl: "/Speakers/young/rama.jpg",
-    name: "Prof. Ramanuja Panigrahi",
-    institution: "Professor, IIT Roorkee",
+    name: "Dr. Ramanuja Panigrahi",
+    institution: "Assistant Professor, IIT Roorkee",
     // talkTitle: "Primary efforts in applying counter manipulation detection technology",
   };
   const speakerData3 = {
     imageUrl: "/Speakers/young/subho.jpg",
-    name: "Prof. Subho Paul",
-    institution: "Professor, IIT BHU",
+    name: "Dr. Subho Paul",
+    institution: "Assistant Professor, IIT BHU",
     // talkTitle: "Primary efforts in applying counter manipulation detection technology",
   };
   const speakerData4 = {
     imageUrl: "/Speakers/young/mayank.jpg",
-    name: "Prof. Mayank Swarnkar",
-    institution: "Professor, IIT BHU",
+    name: "Dr. Mayank Swarnkar",
+    institution: "Assistant Professor, IIT BHU",
     // talkTitle: "Primary efforts in applying counter manipulation detection technology",
   };
 
@@ -37,9 +31,8 @@ function YoungProffesional() {
       </div>
 
       <div className='container mx-auto px-3 py-6 relative z-10'>
-        <h1 className='text-2xl md:text-4xl text-center font-semibold mb-12 backdrop-blur-3xl mx-auto bg-[#8C477E]/20 rounded-2xl'>YOUNG PROFESSIONAL SPEAKERS</h1>
+        <h1 className='text-2xl md:text-4xl text-center font-semibold mb-12 backdrop-blur-3xl mx-auto bg-[#8C477E]/20 rounded-2xl'>YOUNG PROFESSIONAL PANELIST</h1>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-          <SpeakerCard speakerData={speakerData1} />
           <SpeakerCard speakerData={speakerData2} />
           <SpeakerCard speakerData={speakerData3} />
           <SpeakerCard speakerData={speakerData4} />
